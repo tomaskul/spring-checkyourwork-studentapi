25bd19bc0c24583c63f7619d4ec6e02947e5db3f
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-25 15:11:27 +0100

    init project

---
+++ src/test/java/io/github/tomaskul/springcheckyourworkstudentapi/SpringCheckyourworkStudentapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.tomaskul.springcheckyourworkstudentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCheckyourworkStudentapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
