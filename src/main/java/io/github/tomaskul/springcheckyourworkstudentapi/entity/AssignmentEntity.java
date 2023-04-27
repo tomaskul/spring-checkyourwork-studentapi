ccc3fedcddd213deece19c949a4ef70e8bcb1229
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-27 13:50:44 +0100

    Add H2 configuration & seed data.sql with an entity object

---
+++ src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/entity/AssignmentEntity.java
@@ -0,0 +1,96 @@
+package io.github.tomaskul.springcheckyourworkstudentapi.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "CYW_ASSIGNMENTS")
+public class AssignmentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public UUID ID;
+
+    @Column(nullable = false)
+    public String author;
+
+    @Column(nullable = false)
+    public Date publishedDate;
+
+    @Column(nullable = false)
+    public Date dueDate;
+
+    @Column(nullable = false)
+    public String title;
+    public String description;
+    public int creditCount;
+    public boolean isExpired;
+
+    public AssignmentEntity() {
+    }
+
+    public UUID getID() {
+        return ID;
+    }
+
+    public void setID(UUID ID) {
+        this.ID = ID;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCreditCount() {
+        return creditCount;
+    }
+
+    public void setCreditCount(int creditCount) {
+        this.creditCount = creditCount;
+    }
+
+    public boolean isExpired() {
+        return isExpired;
+    }
+
+    public void setExpired(boolean expired) {
+        isExpired = expired;
+    }
+}
