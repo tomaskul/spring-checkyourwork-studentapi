d9ab649d7f67218a0756efede04330fe5a22684a
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-28 10:11:57 +0100

    Add ctor, getters & setters to AssignmentSummary

--- src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/model/AssignmentSummary.java
+++ src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/model/AssignmentSummary.java
@@ -9,4 +9,54 @@ public class AssignmentSummary {
     public boolean isExpired;
     public String title;
     public int creditCount;
+
+    public AssignmentSummary() {}
+
+    public AssignmentSummary(UUID ID, Date dueDate, boolean isExpired, String title, int creditCount) {
+        this.ID = ID;
+        this.dueDate = dueDate;
+        this.isExpired = isExpired;
+        this.title = title;
+        this.creditCount = creditCount;
+    }
+
+    public UUID getID() {
+        return ID;
+    }
+
+    public void setID(UUID ID) {
+        this.ID = ID;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public boolean isExpired() {
+        return isExpired;
+    }
+
+    public void setExpired(boolean expired) {
+        isExpired = expired;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCreditCount() {
+        return creditCount;
+    }
+
+    public void setCreditCount(int creditCount) {
+        this.creditCount = creditCount;
+    }
 }
