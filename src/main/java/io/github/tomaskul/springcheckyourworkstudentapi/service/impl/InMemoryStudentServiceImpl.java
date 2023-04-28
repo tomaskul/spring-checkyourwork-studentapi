0a224366f7f9de16a2153dc08b7aea641d7165e9
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-28 10:14:23 +0100

    Update test to deal with commpiler issues (test needs to be rewritten), updating InMemoryStudentServiceImpl to use student repository instead of in-memory collection (impl rename impending).

--- src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/service/impl/InMemoryStudentServiceImpl.java
+++ src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/service/impl/InMemoryStudentServiceImpl.java
@@ -1,6 +1,9 @@
 package io.github.tomaskul.springcheckyourworkstudentapi.service.impl;
 
+import io.github.tomaskul.springcheckyourworkstudentapi.entity.StudentEntity;
+import io.github.tomaskul.springcheckyourworkstudentapi.repository.StudentRepository;
 import io.github.tomaskul.springcheckyourworkstudentapi.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -10,17 +13,15 @@ import java.util.UUID;
 @Service
 public class InMemoryStudentServiceImpl implements StudentService {
 
-    private final Collection<UUID> students;
+    @Autowired
+    private StudentRepository studentRepository;
 
-    public InMemoryStudentServiceImpl(){
-        students = new ArrayList<>();
-        students.add(UUID.fromString("df36d212-b96c-41d3-b395-f5b793bb9d01"));
-        students.add(UUID.fromString("6ca9daa4-f37f-48ed-ad4e-a50797e3f122"));
-        students.add(UUID.fromString("da1ed158-b630-4046-8338-812b700230af"));
-        students.add(UUID.fromString("de07f7a6-1d12-49b0-a1ad-44b9cf5883d4"));
-    }
+    public Iterable<UUID> getAllIds() {
+        Iterable<StudentEntity> entities = studentRepository.findAll();
 
-    public Collection<UUID> getAllIds() {
-        return students;
+        Collection<UUID> result = new ArrayList<>();
+        entities.forEach(studentEntity -> { result.add(studentEntity.ID); } );
+
+        return result;
     }
 }
--- src/test/java/io/github/tomaskul/springcheckyourworkstudentapi/service/impl/InMemoryStudentServiceImplTests.java
+++ src/test/java/io/github/tomaskul/springcheckyourworkstudentapi/service/impl/InMemoryStudentServiceImplTests.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.Collection;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +18,7 @@ public class InMemoryStudentServiceImplTests {
 
     @Test
     public void returns_4_unique_identifiers(){
-        Collection<UUID> actual = studentService.getAllIds();
+        Iterable<UUID> actual = studentService.getAllIds();
 
         assertThat(actual).isNotNull();
         assertThat(actual).hasSize(4);
