45570d19c2f79237350919d1370c4054a2b62370
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-04-28 10:15:05 +0100

    Update AssignmentController to use services

--- src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/controller/AssignmentController.java
+++ src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/controller/AssignmentController.java
@@ -1,30 +1,42 @@
 package io.github.tomaskul.springcheckyourworkstudentapi.controller;
 
 import io.github.tomaskul.springcheckyourworkstudentapi.model.Assignment;
+import io.github.tomaskul.springcheckyourworkstudentapi.model.AssignmentSummary;
+import io.github.tomaskul.springcheckyourworkstudentapi.service.AssignmentInformationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 @RestController()
 @RequestMapping("/api/v1/assignments/")
 public class AssignmentController {
 
+    @Autowired
+    private AssignmentInformationService assignmentInformationService;
+
     @GetMapping("/all")
-    public ResponseEntity<Collection<Assignment>> getAll(){
-        List<Assignment> assignments = new ArrayList<>();
-        assignments.add(new Assignment());
-        return new ResponseEntity<>(assignments, HttpStatus.OK);
+    public ResponseEntity<Iterable<AssignmentSummary>> getAll(){
+        // TODO: get student UUID from request header.
+        Iterable<AssignmentSummary> summaries = assignmentInformationService.getAssignedSummaries(UUID.randomUUID());
+        if (summaries == null) {
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        }
+
+        return new ResponseEntity<>(summaries, HttpStatus.OK);
     }
 
     @GetMapping("/byId/{id}")
     public ResponseEntity<Assignment> getById(@PathVariable String id){
-        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        Assignment entity = assignmentInformationService.getById(UUID.fromString(id));
+        if (entity == null) {
+            return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(entity, HttpStatus.OK);
     }
 
     @PostMapping("/upload/{assignmentId}")
