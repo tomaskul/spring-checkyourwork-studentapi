41c956c25a9cc68bd72893fbad2bb7ecddcbf225
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-05-09 10:44:08 +0100

    Add basic swagger docs

--- src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/SpringCheckyourworkStudentapiApplication.java
+++ src/main/java/io/github/tomaskul/springcheckyourworkstudentapi/SpringCheckyourworkStudentapiApplication.java
@@ -1,7 +1,12 @@
 package io.github.tomaskul.springcheckyourworkstudentapi;
 
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCheckyourworkStudentapiApplication {
@@ -10,4 +15,15 @@ public class SpringCheckyourworkStudentapiApplication {
 		SpringApplication.run(SpringCheckyourworkStudentapiApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI publicAPI() {
+		return new OpenAPI()
+				.info(new Info().title("CheckYourWork - Student API")
+						.description("Student API for CheckYourWork kata implementation.")
+						.version("v0.0.1")
+						/*.license(new License().name("Apache 2.0").url("http://springdoc.org"))*/)
+				/*.externalDocs(new ExternalDocumentation()
+						.description("SpringShop Wiki Documentation")
+						.url("https://springshop.wiki.github.org/docs"))*/;
+	}
 }
